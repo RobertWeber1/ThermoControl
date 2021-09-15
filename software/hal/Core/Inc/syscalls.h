2bc90c72fedd9fa10add64d2c11a9dcfa827c817
Robert Weber <robert.weber.1@gmx.net>
2021-09-15 17:24:54 +0200

    Working UART and SPI

---
+++ software/hal/Core/Inc/syscalls.h
@@ -0,0 +1,19 @@
+#pragma once
+#include "stm32g0xx_hal.h"
+#include <sys/stat.h>
+
+extern "C" {
+
+void RetargetInit(UART_HandleTypeDef *huart);
+int _isatty(int fd);
+int _write(int fd, char* ptr, int len);
+int _close(int fd);
+int _lseek(int fd, int ptr, int dir);
+int _read(int fd, char* ptr, int len);
+int _fstat(int fd, struct stat* st);
+caddr_t _sbrk (int incr);
+
+}
+
+// All credit to Carmine Noviello for this code
+// https://github.com/cnoviello/mastering-stm32/blob/master/nucleo-f030R8/system/src/retarget/retarget.c
